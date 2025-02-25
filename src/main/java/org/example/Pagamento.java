dbd38a387f0ae3e97a6db0a667cc1c321ebe06f2
Carolina Eskenazi <carolinae3@al.insper.edu.br>
2025-02-24 21:06:09 -0300

    funcoes pizza e pagamento com objetos e funcoes get e set

--- src/main/java/org/example/Pagamento.java
+++ src/main/java/org/example/Pagamento.java
@@ -1,4 +1,45 @@
 package org.example;
 
 public class Pagamento {
+
+    private double valor;
+    private boolean pago;
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public boolean isPago() {
+        return pago;
+    }
+
+    public void setPago(boolean pago) {
+        this.pago = pago;
+    }
+
+    public String realizarPagamento(double valor_pizza,double dinheiro){
+
+        if (dinheiro >= valor_pizza){
+            double troco = dinheiro - valor_pizza;
+            return "O seu troco foi de" + troco;
+        }
+        else{
+            return  "Não foi possível concluir o pagamento!";
+
+        }
+
+
+    }
+
+
+
+
+
+
+
+
 }
--- src/main/java/org/example/Pizza.java
+++ src/main/java/org/example/Pizza.java
@@ -1,4 +1,38 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Pizza {
+
+    private String sabor;
+    private ArrayList<String> igredientes = new ArrayList<>();
+    private double Preco;
+
+    public String getSabor() {
+        return sabor;
+    }
+
+    public void setSabor(String sabor) {
+        this.sabor = sabor;
+    }
+
+    public ArrayList<String> getIgredientes() {
+        return igredientes;
+    }
+
+    public void setIgredientes(ArrayList<String> igredientes) {
+        this.igredientes = igredientes;
+    }
+
+    public double getPreco() {
+        return Preco;
+    }
+
+    public void setPreco(double preco) {
+        Preco = preco;
+
+    }
+
+
+
 }
